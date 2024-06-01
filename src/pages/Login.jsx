a14884f07e9dff0830f011f217f814d2805a33c6
agrawalhimani@gmail.com <agrawalhimani146@gamil.com>
2024-06-01 09:54:53 +0545

    msg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom';
+
 const Login = () => {
+  const [data, setData] = useState({
+    username: "",
+    password: ""
+  });
+const navigate= useNavigate();
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+     await axios.post("login", data);
+     navigate("/")
+     
+    } catch (err) {
+      console.log(err);
+      setErr(err.response.data);
+    }
+  };
+
   return (
     <div className='auth'>
-        <h1>Login</h1>
-      <form>
-        <input type="text" placeholder='UserName' required />
-        <input type="password" placeholder='Password' required />
-        <button>Login</button>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Username' name='username' required onChange={handleChange} />
+        <input type="password" placeholder='Password' name="password" required onChange={handleChange} />
+        <button type="submit">Login</button>
         <span>Not Yet Registered?</span> <Link to="/register" className="link">Register</Link>
+        {err && <p style={{ color: "red" }}>{err}</p>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+   const navigate= useNavigate();
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = e => {
+    setData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+       await axios.post("http://192.168.1.90:8800/app/register", data);
+       navigate("/login")
+      
+    } catch (err) {
+      console.log(err);
+      setErr(err.response.data);
+    }
+  };
+
   return (
     <div className='auth'>
       <h1>Register</h1>
-      <form> 
-        <input type="text" placeholder='Username' required />
-        <input type="email" placeholder='Email' required />
-        <input type="text" placeholder="Moblie-Number" required />
-        <input type="password" placeholder='Password' required />
-        <button>Submit</button>
-
+      <form onSubmit={handleSubmit}> 
+        <input type="text" placeholder='Username' name='username' value={data.username} onChange={handleChange} required />
+        <input type="email" placeholder='Email' name="email" value={data.email} onChange={handleChange} required />
+        <input type="password" placeholder='Password' name='password' value={data.password} onChange={handleChange} required />
+        <button type="submit">Submit</button>
+        {err && <p style={{ color: "red" }}>{err}</p>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
